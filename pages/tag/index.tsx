99a80972af7f4d0cd1afc48a6607315ce08a11e1
KayzerCode <zlo.alien@gmail.com>
2025-04-27 14:05:12 +0300

    Vercel Analitycs

--- pages/tag/index.tsx
+++ pages/tag/index.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import Link from "next/link";
 import productsData from "@/products/products.json";
 import { toUrlSlug } from "@/utils/slugify";
